1688bf88413b87cd5884bd4b15a8671f75e51140
Donald <johndoe@example.com>
2022-10-09 01:34:07 +0300

    1задача

---
+++ 6-1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите число М и потом такое же число произвольных чисел:");
+int numberM = int.Parse(Console.ReadLine()!);
+int counter = 0;
+
+for (int i = 0; i < numberM; i++)
+{
+    int thisNumber = int.Parse(Console.ReadLine()!);
+    if (thisNumber > 0)
+    {
+        counter ++;
+    }
+    
+}
+Console.WriteLine($"Количество положительных чисел: {counter}");
